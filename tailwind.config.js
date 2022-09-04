864e77a83634de86c7a0c70dc230499939d2385e
rhmti01 <rahmati.business@gmail.com>
2022-09-05 00:45:04 +0430

    starting sort and filter bar in mobile section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ["iransansdn"],
+      },
+    },
+  },
+  plugins: [],
+}
