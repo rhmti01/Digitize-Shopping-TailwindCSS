818c37fd3e928dfe9dcd37b92d8f909afe08a92e
rhmti01 <rahmati.business@gmail.com>
2022-09-17 02:09:43 +0430

    side bar added to product page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,10 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    }),
+  ],
 }
