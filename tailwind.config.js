4506233204ea9d092fa6330d8af179f29e9e45f1
rhmti01 <rahmati.business@gmail.com>
2023-07-28 14:30:24 +0330

    main page of products has been finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,31 @@ module.exports = {
   content: ["./public/**/*.html"],
   theme: {
     extend: {
+      screens: {
+        'it':'300px',
+       'shit':'363px',
+      },
       fontFamily: {
-        'sans': ["iransansdn"],
+        'sans': ["yekanbakh"],
       },
       container: {
         center: true,
       },
       screens: {
-        'xx': ' 200px ',
         'sx': ' 333px ',
+        'sv': '360px',
+        'ww':' 470px ',
+        'mm': '500px',
         'ss': ' 555px ',
+        'rr':'650px',
+        'zz': '700px',
+        'jj' : '767px ',
+        'xxl':'1281px',
         '3xl': '1850px',
         '4xl': '1921px',
+      },
+      lineHeight: {
+        'extra-loose': '6.5',
       },
     },
   },
